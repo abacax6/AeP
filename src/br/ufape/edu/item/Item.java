b5ce879fa0b6241282dd1dc98f11adf5430b2d0a
Paulo Cardoso <pouvini5@gmail.com>
2025-04-02 22:11:11 -0300

    Criando e Inicializando a Classe Item. - Paulo

--- src/br/ufape/edu/item/Item.java
+++ src/br/ufape/edu/item/Item.java
@@ -1,5 +1,64 @@
 package br.ufape.edu.item;
 
-public class Item {
+public class Item{
+
+	private String nome;
+	private String discricao;
+	private String contato; 
+	private String img;
+	private int id;
+	
+	
+	public Item(String nome, String discricao, String contato, String img, int id) {
+		
+		this.nome = nome;
+		this.discricao = discricao;
+		this.contato = contato;
+		this.img = img;
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDiscricao() {
+		return discricao;
+	}
+
+	public void setDiscricao(String discricao) {
+		this.discricao = discricao;
+	}
+
+	public String getContato() {
+		return contato;
+	}
+
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	
+	@Override
+    public String toString() {
+        return id + ";" + nome + ";" + discricao + ";" + contato + ";" + img;
+    }
+
+	
+
+	
+	
 
 }
