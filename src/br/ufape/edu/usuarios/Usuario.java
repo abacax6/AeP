0ead02d3c41c4adfae660549947971fcf1c18efa
AbacaX <victorraze@protonmail.com>
2025-04-02 22:31:27 -0300

    Classes Usuario e Solicitacao

--- src/br/ufape/edu/usuarios/Usuario.java
+++ src/br/ufape/edu/usuarios/Usuario.java
@@ -1,5 +1,56 @@
 package br.ufape.edu.usuarios;
 
-public class Usuario {
+import java.util.ArrayList;
+import br.ufape.edu.solicitacao.Solicitacao;
 
+public class Usuario {
+	// ATRIBUTOS
+	private int id;
+	private String nome;
+	private String senha;
+	private String contato;
+	private ArrayList<Solicitacao> solicitacoes;
+	
+	
+	// CONSTRUTOR
+	public Usuario(int id, String nome, String senha, String contato, ArrayList<Solicitacao> solicitacoes) {
+		this.id = id;
+		this.nome = nome;
+		this.senha = senha;
+		this.contato = contato;
+		this.solicitacoes = solicitacoes;
+	}
+	
+	// GETTERS E SETTERS
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getContato() {
+		return contato;
+	}
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+	public ArrayList<Solicitacao> getSolicitacoes() {
+		return solicitacoes;
+	}
+	public void setSolicitacoes(ArrayList<Solicitacao> solicitacoes) {
+		this.solicitacoes = solicitacoes;
+	}
+	
 }
