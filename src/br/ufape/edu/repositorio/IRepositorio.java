b9d3036f14a5c24b2adeb17fa94bf589f789860c
Paulo Cardoso <pouvini5@gmail.com>
2025-04-03 21:08:39 -0300

    Interface Repositorio

---
+++ src/br/ufape/edu/repositorio/IRepositorio.java
@@ -0,0 +1,28 @@
+package br.ufape.edu.repositorio;
+
+import java.util.ArrayList;
+
+import br.ufape.edu.item.Item;
+import br.ufape.edu.solicitacao.Solicitacao;
+import br.ufape.edu.usuarios.Usuario;
+
+public interface IRepositorio {
+
+	void cadastrarUsuario(Usuario usuario);
+	Usuario bucaUsuarioPorEmail(String email);
+	ArrayList<Usuario> listarUsuarios();
+
+	void cadastrarItem(Item item);
+	void moverParaResgatados(Item item);
+	ArrayList<Item> listarItensAnunciados();
+	ArrayList<Item> listarItensResgatados();
+	Item buscarItemPorId(int id);
+	
+
+	void cadastrarSolicitacao(Solicitacao solicitacao);
+	void aprovarSolicitacao(Solicitacao solicitacao);
+	void descartarSolicitacao(Solicitacao solicitacao);
+	ArrayList<Solicitacao> listarSolicitacoesPendentes();
+	ArrayList<Solicitacao> listarTodasSolicitacoes();
+
+}
